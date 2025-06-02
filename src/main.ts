8d4a7ddd595cdf0fe945fb89d9f5852900265d96
Giulianna Ribeiro <giuliannaribeiro00@gmail.com>
2025-06-02 17:21:54 -0300

    chore: ajustes iniciais no setup do projeto

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideHttpClient } from '@angular/common/http';
+import { importProvidersFrom, LOCALE_ID } from '@angular/core';
 import { AppComponent } from './app/app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt, 'pt-BR');
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(BrowserAnimationsModule),
+    provideHttpClient(),
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
+  ]
+}).catch((err) => console.error(err));
